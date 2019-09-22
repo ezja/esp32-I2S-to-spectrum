44cf35483ebbc99c98c5bbfd6ac93a04826c0277
ezja <55329507+ezja@users.noreply.github.com>
2019-09-22 19:41:55 +0200

    Update i2s_fft.h

--- i2s_fft.h
+++ i2s_fft.h
@@ -13,6 +13,28 @@
 // 510 Hz @ BLOCK_SIZE 2048 & SAMPLERATE 6250 kHz
 #define ENDBIN 82 
 
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "driver/i2s.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_spiffs.h"
+#include "esp_system.h"
+#include "esp_sleep.h"
+#include "esp_dsp.h"
+#include "dsp_platform.h"
+#include "dsps_view.h"
+#include "dsps_fft2r.h"
+#include "freertos/ringbuf.h"
+#include "freertos/semphr.h"
+#include "esp_heap_caps.h"
+
 // ***********************************
 // s_spectrum is de LoRaWAN payload!
 //
